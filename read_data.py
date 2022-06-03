da92e0476e0b49cc5fe4e1b37a17064006779b75
Alexander Laß <alex.lass@t-online.de>
2022-06-03 14:30:21 +0200

    Improved diagrams and introduced calculation of confedence interval

--- read_data.py
+++ read_data.py
@@ -1,6 +1,7 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import correlate, find_peaks
+from scipy.stats import norm
 
 
 folder = "../Messungen/Testmessungen"
@@ -11,6 +12,16 @@ def linear_interpolation_x(y,y1,y0,x1,x0):
         print("Warning: y should be between y0 and y1")
 
     return x0+(( (y-y0) * (x1-x0) )/(y1-y0))
+
+def confidence_interval(data, percentage):
+    quantile = (percentage + 1)/2
+    mean = np.mean(data)
+    var = np.var(data)
+    n = len(data)
+    sigma = norm.ppf(quantile)
+    HIB = sigma * np.sqrt(var/n)
+    return mean, np.array([mean - HIB, mean + HIB])
+
 
 def read_meta_data(filename):
     str = filename.split('_')
@@ -27,12 +38,12 @@ def read_meta_data(filename):
 if __name__ == "__main__":
     fc, fs, batchsize, capture_interval = read_meta_data(file)
 
-    data = numpy.fromfile(open(f"{folder}/{file}"), dtype=numpy.complex64)
-    zc_seq = numpy.load('zc_sequence.npy')
+    data = np.fromfile(open(f"{folder}/{file}"), dtype=np.complex64)
+    zc_seq = np.load('zc_sequence.npy')
 
     corr = correlate(data, zc_seq, mode = "full")
-    corr_max = numpy.max(corr)
-    start_of_first_batch = numpy.argmax(corr>corr_max*0.1)
+    corr_max = np.max(corr)
+    start_of_first_batch = np.argmax(corr>corr_max*0.1)
 
     recieved_power = []
     number_of_batches = int( (len(data) - start_of_first_batch) / batchsize )
@@ -41,34 +52,36 @@ if __name__ == "__main__":
         lower = start_of_first_batch + batchsize * b
         upper = start_of_first_batch + batchsize * (b+1)
         #calculate the recieved power
-        recieved_power.append(numpy.sum(abs(data[lower:upper]**2)))
+        recieved_power.append(np.sum(abs(data[lower:upper]**2)))
         #seperate the batches
         batches.append(corr[lower:upper])
 
     #create a time axis
-    time = numpy.arange(0,capture_interval*number_of_batches, capture_interval)
+    time = np.arange(0,capture_interval*number_of_batches, capture_interval)
     #plot the recieved power over time
+    plt.figure(num="P(t)")
     plt.plot(time, recieved_power)
     plt.xlabel("Time in [s]")
     plt.ylabel("Power of detected signal")
-    plt.ylim([0, numpy.max(recieved_power)*1.02])
-    plt.show()
+    plt.title("Recieved signal power over time")
+    plt.ylim([0, np.max(recieved_power)*1.02])
+    #plt.show()
 
     h_meas = []
     b=0
     for batch in batches:
-        batch_max = numpy.max(batch)
+        batch_max = np.max(batch)
         xpeaks = find_peaks(abs(batch), 0.9 * batch_max, distance = 200)[0]
         ypeaks = []
         h=[]
         for peak in xpeaks:
             peak_ =peak+b*batchsize
             h.append(corr[peak_-10:peak_+118])
-        h_meas.append( numpy.mean(h[1:], axis =0))
+        h_meas.append( np.mean(h[1:], axis =0))
         b+=1
     T = [] # timevariant transferfunction
     for h in h_meas:
-        T.append(abs(numpy.fft.fftshift(numpy.fft.fft(h))))
+        T.append(abs(np.fft.fftshift(np.fft.fft(h))))
 
     #for t in T:
         #plt.plot(abs(t))
@@ -76,25 +89,30 @@ if __name__ == "__main__":
     B_coh_50 = []
     B_coh_90 = []
     for t in range(len(T)):
-        freq_corr_function = numpy.fft.fftshift( correlate(T[t],T[t], mode = "full") )
+        freq_corr_function = np.fft.fftshift( correlate(T[t],T[t], mode = "full") )
         delta_x = 2*(fs/10**6)/len(freq_corr_function)
-        freq_corr_max = abs( numpy.max(freq_corr_function) )
+        freq_corr_max = abs( np.max(freq_corr_function) )
 
-        x1 = ( numpy.argmax( abs(freq_corr_function) < 0.5 * freq_corr_max ))
+        x1 = ( np.argmax( abs(freq_corr_function) < 0.5 * freq_corr_max ))
         x0 = x1 - 1
         B_coh_50.append( linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_x )
         #print(f"x0 = {x0}, x1 = {x1}, y0 = {abs(freq_corr_function[x0])}, y1 = {abs(freq_corr_function[x1])}, y = {0.5 * freq_corr_max}, x = {linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0)}")
 
-        x1 = ( numpy.argmax( abs(freq_corr_function) < 0.9 * freq_corr_max ))
+        x1 = ( np.argmax( abs(freq_corr_function) < 0.9 * freq_corr_max ))
         x0 = x1 - 1
         B_coh_90.append( linear_interpolation_x(0.9 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1*delta_x, x0*delta_x) )
 
-
+    plt.figure(num="B_coh(t)")
     plt.plot(time, B_coh_50, "-b", label = 'B_coh_50%(t)' )
     plt.plot(time, B_coh_90, "-r", label = 'B_coh_90%(t)' )
     plt.xlabel("Time in [s]")
     plt.ylabel("B_coh in [MHz]")
+    plt.title("Coherence Bandwidths over time")
     plt.legend(loc="best")
-    plt.show()
 
-    
+    B_coh_50_mean, B_coh_50_conf_interval_999 = confidence_interval(B_coh_50, 0.999)
+    B_coh_90_mean, B_coh_90_conf_interval_999 = confidence_interval(B_coh_90, 0.999)
+    print(f'99.9% of values of B_coh_50 lie in between {B_coh_50_conf_interval_999}[Mhz]')
+    print(f'99.9% of values of B_coh_90 lie in between {B_coh_90_conf_interval_999}[MHz]')
+
+    plt.show()
