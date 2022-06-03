a4f4f2223892683d562d6f5b2c623c2aac9736ae
Alexander Laß <alex.lass@t-online.de>
2022-06-03 17:10:32 +0200

    Add calculation for T_coh(WIP)

--- read_data.py
+++ read_data.py
@@ -8,7 +8,7 @@ folder = "../Messungen/Testmessungen"
 file = "capture_2022-06-01_10-38-59_3750MHz_20MSps_2048S_10ms.dat"
 
 def linear_interpolation_x(y,y1,y0,x1,x0):
-    if not y0 >= y >= y1:
+    if not y0 >= y >= y1 and not y0 <= y <= y1:
         print("Warning: y should be between y0 and y1")
 
     return x0+(( (y-y0) * (x1-x0) )/(y1-y0))
@@ -79,6 +79,7 @@ if __name__ == "__main__":
             h.append(corr[peak_-10:peak_+118])
         h_meas.append( np.mean(h[1:], axis =0))
         b+=1
+
     T = [] # timevariant transferfunction
     for h in h_meas:
         T.append(abs(np.fft.fftshift(np.fft.fft(h))))
@@ -86,21 +87,23 @@ if __name__ == "__main__":
     #for t in T:
         #plt.plot(abs(t))
     #plt.show()
+
+    #B_coh
     B_coh_50 = []
     B_coh_90 = []
     for t in range(len(T)):
         freq_corr_function = np.fft.fftshift( correlate(T[t],T[t], mode = "full") )
-        delta_x = 2*(fs/10**6)/len(freq_corr_function)
+        delta_f = 2*(fs/10**6)/len(freq_corr_function)
         freq_corr_max = abs( np.max(freq_corr_function) )
 
         x1 = ( np.argmax( abs(freq_corr_function) < 0.5 * freq_corr_max ))
         x0 = x1 - 1
-        B_coh_50.append( linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_x )
+        B_coh_50.append( linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_f )
         #print(f"x0 = {x0}, x1 = {x1}, y0 = {abs(freq_corr_function[x0])}, y1 = {abs(freq_corr_function[x1])}, y = {0.5 * freq_corr_max}, x = {linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0)}")
 
         x1 = ( np.argmax( abs(freq_corr_function) < 0.9 * freq_corr_max ))
         x0 = x1 - 1
-        B_coh_90.append( linear_interpolation_x(0.9 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1*delta_x, x0*delta_x) )
+        B_coh_90.append( linear_interpolation_x(0.9 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_f )
 
     plt.figure(num="B_coh(t)")
     plt.plot(time, B_coh_50, "-b", label = 'B_coh_50%(t)' )
@@ -114,5 +117,35 @@ if __name__ == "__main__":
     B_coh_90_mean, B_coh_90_conf_interval_999 = confidence_interval(B_coh_90, 0.999)
     print(f'99.9% of values of B_coh_50 lie in between {B_coh_50_conf_interval_999}[Mhz]')
     print(f'99.9% of values of B_coh_90 lie in between {B_coh_90_conf_interval_999}[MHz]')
+
+    #T_coh
+
+    T = np.swapaxes(T,0,1)
+    T_coh_50 = []
+    T_coh_90 = []
+    resolution_in_time = 1 #100ms
+    batches_per_value = int(resolution_in_time/capture_interval)
+    for t in range (0, number_of_batches, batches_per_value):
+        T_coh_50_f = []
+        T_coh_90_f = []
+        for f in range(len(T)):
+            time_corr_function = np.fft.fftshift( correlate(T[f][t:t+batches_per_value],T[f][t:t+batches_per_value], mode = "full") )
+
+            delta_t = 2*resolution_in_time/len(time_corr_function)
+            time_corr_max = abs( np.max(time_corr_function) )
+
+            x1 = ( np.argmax( abs(time_corr_function) < 0.5 * time_corr_max ))
+            x0 = x1 - 1
+            T_coh_50_f.append(linear_interpolation_x(0.5 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x0]), x1, x0) * delta_t )
+
+            x1 = ( np.argmax( abs(time_corr_function) < 0.9 * time_corr_max ))
+            x0 = x1 - 1
+            T_coh_90_f.append( linear_interpolation_x(0.9 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x0]), x1, x0) * delta_t )
+        T_coh_50.append(np.mean(T_coh_50_f))
+        T_coh_90.append(np.mean(T_coh_90_f))
+
+    plt.figure()
+    plt.plot(T_coh_50, "-b", label = 'T_coh_50%(t)' )
+    plt.plot(T_coh_90, "-r", label = 'T_coh_90%(t)' )
 
     plt.show()
