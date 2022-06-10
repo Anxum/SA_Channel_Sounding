Add 3D Plot of Transfer function, threw away samples affected by lowpass, used fixed axis for better comparison between different measurements, Add moving average filter over B_Coh, plotted recieved power in dBFS

read_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import correlate, find_peaks
 from scipy.stats import norm
+from mpl_toolkits.mplot3d import Axes3D
 
 
 folder = "../Messungen/Testmessungen"
@@ -31,7 +32,7 @@ def read_meta_data(filename):
     capture_interval_ = int( str[6][0:str[6].find('ms')] ) * 10**-3
     print(f'Date of measurement = {str[1]}')
     print(f'Time of measurement = {str[2]}')
-    print(f'fc = {str[3]}, fs = {str[4]}, batchsize = {str[5]}, capture interval = {str[6][:-3]}')
+    print(f'fc = {fc_ * 10**-6}MHz, fs = {fs_ * 10**-6}MHz, batchsize = {batchsize_}S, capture interval = {capture_interval_ * 10**3}ms')
     return fc_, fs_, batchsize_, capture_interval_
 
 
@@ -39,15 +40,9 @@ if __name__ == "__main__":
     fc, fs, batchsize, capture_interval = read_meta_data(file)
     data = np.fromfile(open(f"{folder}/{file}"), dtype=np.complex64)
     zc_seq = np.load('zc_sequence.npy')
-    # TODO: Rechteckfelter, um Tp effekte zu anullieren
-    # TODO: Moving avrage über B_coh
     # Todo: andere Szenarien testen, zB. 30-40 Mhz Bandbreite
     # Todo: Zielband 1785 - 1805 MHz --> >20 MHz 30.72 MHz = LTE
-    #Todo: Revieved power in db
-    #TOdo: "anfang der Sequenz Algorithmus" ändern/entfernen
     # TODO: Frequenzantworten bei den "komischen" Ausreißern ansehen --> Untersuchen der Störung
-    #TODO: Fixe Achsen bei den Plots zum Vergleich zwischen den Messungen (max BCoh = fs, Max T_coh = window size)
-    # TOdo: 3D-Frequenzplot von T(t,f)
     #TODO: cfo correction - Paper siehe Nick
 
 
@@ -55,7 +50,9 @@ if __name__ == "__main__":
     corr_max = np.max(corr)
 
     recieved_power = []
-    number_of_batches = int( (len(data)) / batchsize ) #TODO: ovrflow check (number of batches not int)
+    if len(data) % batchsize:
+        print("There has been an over or underflow. Please repeat the measurement!") # maybe raise an exception
+    number_of_batches = int( (len(data)) / batchsize )
     batches = []
     for b in range(0,number_of_batches):
         lower = batchsize * b
@@ -67,13 +64,13 @@ if __name__ == "__main__":
 
     #create a time axis
     time = np.arange(0,capture_interval*number_of_batches, capture_interval)
+    recieved_power_dbfs = 10 * np.log10(recieved_power)
     #plot the recieved power over time
     plt.figure(num="P(t)")
-    plt.plot(time, recieved_power)
+    plt.plot(time, recieved_power_dbfs)
     plt.xlabel("Time in [s]")
-    plt.ylabel("Power of detected signal")
+    plt.ylabel("Power of detected signal in [dBFS]")
     plt.title("Recieved signal power over time")
-    plt.ylim([0, np.max(recieved_power)*1.02])
     #plt.show()
 
     h_meas = []
@@ -81,59 +78,78 @@ if __name__ == "__main__":
     for batch in batches:
         batch_max = np.max(batch)
         xpeaks = find_peaks(abs(batch), 0.9 * batch_max, distance = 200)[0]
-        ypeaks = []
         h=[]
         for peak in xpeaks:
-            peak_ =peak+b*batchsize
+            peak_ =peak + b*batchsize
             h.append(corr[peak_-10:peak_+118])
         h_meas.append( np.mean(h[1:], axis =0))
         b+=1
+    h_meas = np.array(h_meas)
 
     T = [] # timevariant transferfunction
     for h in h_meas:
-        T.append(abs(np.fft.fftshift(np.fft.fft(h))))
+        T.append(abs(np.fft.fftshift(np.fft.fft(h)))) # 128 point-fft
+    T = np.array(T)
+    #Cut Lowpass filter effects away
+    T = T[:, 5:-5]
+    cutoff_frequency = (1 - ( (1-len(T[0])) / 128 ) *fs)/2
+    #create a frequency axis
+    frequency = np.linspace( -cutoff_frequency, cutoff_frequency, len(T[0]))
+    f, t = np.meshgrid(frequency, time)
+    #plot transferfunction over time and frequency
+    fig = plt.figure(num = "T(t,f)")
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_xlabel("Time in [s]")
+    ax.set_ylabel("Frequency in [Hz]")
+    ax.set_zlabel("T(t,f)")
+    ax.plot_surface(t, f, abs(T), cmap="plasma")
 
-    #for t in T:
-        #plt.plot(abs(t))
-    #plt.show()
+
 
     #B_coh
     B_coh_50 = []
     B_coh_90 = []
     for t in range(len(T)):
         freq_corr_function = np.fft.fftshift( correlate(T[t],T[t], mode = "full") )
-        delta_f = 2*(fs/10**6)/len(freq_corr_function)
+        delta_f = 2*(fs)/len(freq_corr_function)
         freq_corr_max = abs( np.max(freq_corr_function) )
 
         x1 = ( np.argmax( abs(freq_corr_function) < 0.5 * freq_corr_max ))
-        x0 = x1 - 1
-        B_coh_50.append( linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_f )
-        #print(f"x0 = {x0}, x1 = {x1}, y0 = {abs(freq_corr_function[x0])}, y1 = {abs(freq_corr_function[x1])}, y = {0.5 * freq_corr_max}, x = {linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0)}")
+        B_coh_50.append( linear_interpolation_x(0.5 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x1 - 1]), x1, x1 - 1) * delta_f )
 
         x1 = ( np.argmax( abs(freq_corr_function) < 0.9 * freq_corr_max ))
-        x0 = x1 - 1
-        B_coh_90.append( linear_interpolation_x(0.9 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x0]), x1, x0) * delta_f )
+        B_coh_90.append( linear_interpolation_x(0.9 * freq_corr_max, abs(freq_corr_function[x1]), abs(freq_corr_function[x1 - 1]), x1, x1 - 1) * delta_f )
+
+    #Moving average filter
+    windowsize_in_sec = 1
+    batches_per_value = round(windowsize_in_sec/capture_interval)
+    N = capture_interval * number_of_batches
+    M = batches_per_value * capture_interval
+    B_coh_50 = np.convolve(B_coh_50, np.ones(batches_per_value)/batches_per_value, mode = 'valid')
+    B_coh_90 = np.convolve(B_coh_90, np.ones(batches_per_value)/batches_per_value, mode = 'valid')
+    time = np.linspace(M/2, N-M/2, len(B_coh_50))
 
     plt.figure(num="B_coh(t)")
     plt.plot(time, B_coh_50, "-b", label = 'B_coh_50%(t)' )
     plt.plot(time, B_coh_90, "-r", label = 'B_coh_90%(t)' )
     plt.xlabel("Time in [s]")
-    plt.ylabel("B_coh in [MHz]")
-    plt.title("Coherence Bandwidths over time")
+    plt.ylabel("B_coh in [Hz]")
+    plt.title("Coherence bandwidths over time with moving average filter applied")
+    plt.ylim([0, fs])
     plt.legend(loc="best")
 
     B_coh_50_mean, B_coh_50_conf_interval_999 = confidence_interval(B_coh_50, 0.999)
     B_coh_90_mean, B_coh_90_conf_interval_999 = confidence_interval(B_coh_90, 0.999)
-    print(f'99.9% of values of B_coh_50 lie in between {B_coh_50_conf_interval_999}[Mhz]')
-    print(f'99.9% of values of B_coh_90 lie in between {B_coh_90_conf_interval_999}[MHz]')
+    print(f'99.9% of values of B_coh_50 lie in between {B_coh_50_conf_interval_999}[Hz]')
+    print(f'99.9% of values of B_coh_90 lie in between {B_coh_90_conf_interval_999}[Hz]')
 
     #T_coh
 
     T = np.swapaxes(T,0,1)
     T_coh_50 = []
     T_coh_90 = []
-    windowsize_in_sec = 1 #1s
-    batches_per_value = int(windowsize_in_sec/capture_interval)
+    windowsize_in_sec = 1
+    batches_per_value = round(windowsize_in_sec/capture_interval)
     for t in range (0, number_of_batches, batches_per_value):
         T_coh_50_f = []
         T_coh_90_f = []
@@ -144,17 +160,21 @@ if __name__ == "__main__":
             time_corr_max = abs( np.max(time_corr_function) )
 
             x1 = ( np.argmax( abs(time_corr_function) < 0.5 * time_corr_max ))
-            x0 = x1 - 1
-            T_coh_50_f.append(linear_interpolation_x(0.5 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x0]), x1, x0) * delta_t )
+            T_coh_50_f.append(linear_interpolation_x(0.5 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x1 - 1]), x1, x1 - 1) * delta_t )
 
             x1 = ( np.argmax( abs(time_corr_function) < 0.9 * time_corr_max ))
-            x0 = x1 - 1
-            T_coh_90_f.append( linear_interpolation_x(0.9 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x0]), x1, x0) * delta_t )
+            T_coh_90_f.append( linear_interpolation_x(0.9 * time_corr_max, abs(time_corr_function[x1]), abs(time_corr_function[x1 - 1]), x1, x1 - 1) * delta_t )
+
         T_coh_50.append(np.mean(T_coh_50_f))
         T_coh_90.append(np.mean(T_coh_90_f))
 
-    plt.figure()
-    plt.plot(T_coh_50, "-b", label = 'T_coh_50%(t)' )
-    plt.plot(T_coh_90, "-r", label = 'T_coh_90%(t)' )
-
+    time = np.arange(0, number_of_batches * capture_interval, windowsize_in_sec)
+    plt.figure(num="T_coh(t)")
+    plt.plot(time, T_coh_50, "-b", label = 'T_coh_50%(t)' )
+    plt.plot(time, T_coh_90, "-r", label = 'T_coh_90%(t)' )
+    plt.xlabel("Time in [s]")
+    plt.ylabel("T_coh in [s]")
+    plt.ylim([0, windowsize_in_sec])
+    plt.title("Coherence times over time")
+    plt.legend(loc="best")
     plt.show()